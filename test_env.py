a337b51b84d674bd00eb79f88df9b864ef460c06
Griffith-Stites <gjstites@gmail.com>
2019-02-01 21:13:30 -0500

    Added more string notes

--- test_env.py
+++ test_env.py
@@ -1,10 +1,27 @@
 #For testing items for gene_finder
 
-string = "hello world"
-len(string)
-print(len(string))
+string = "hello world2 world1"
+print(string)
 test = string[1]
 print(test)
 print(type("A"))
-string_n = string.replace("world ","")
+string_n = string.replace("world","")
 print(string_n)
+
+
+def find(word, letter):
+    index = 0
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = index + 1
+    return -1
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count)
+
+for i in range(len(numbers)):
+    numbers[i] = numbers[i] * 2
